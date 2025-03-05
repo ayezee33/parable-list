c3a6489a25a50f6c71b6d22b06a917accdb4bf43
Alex Zerbach <alex.zerbach@gmail.com>
2025-03-04 21:23:49 -0800

    add sitemap and robots txt for realsy

---
+++ out/_next/static/1ujw035kR3ATowjgaXvCs/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fcategories","\u002Fcategories\u002F[category]","\u002Fparables","\u002Fparables\u002F[testament]\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
